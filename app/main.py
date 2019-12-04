33c662836b5b819e55b1ae80141713418140c4db
myciek <maciejmycielski@gmail.com>
2019-12-04 10:30:00 +0100

    Revert "Category model added"
    This reverts commit 7758b8e8

--- app/main.py
+++ app/main.py
@@ -1,16 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    category_name = db.Column(db.String(80), unique = True, nullable = False)
-
-    def __repr__(self):
-        return self.category_name
-
 
 @app.route('/')
 def index():
