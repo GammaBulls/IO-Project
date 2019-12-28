d9b9c4e93bfdb27f2d9f619fd06a3f4d2220d3d3
Karol Jaksik <jaksikkarol@gmail.com>
2019-12-28 19:13:02 +0100

    Category crud operations (#3)
    * Update gitignore
    
    * Add crud operations to category
    
    * Update requirements
    
    * Update models

--- app/main.py
+++ app/main.py
@@ -1,94 +1,150 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+import enum
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
 class Category(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    category_name = db.Column(db.String(80), unique = True, nullable = False)
-    advertisements = db.relationship("Advertisement", backref="category", lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(80), unique=True, nullable=False)
 
-    def __repr__(self):
-        return self.category_name
-
-class TerminationReason(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    termination_reason = db.Column(db.String(80), unique = True, nullable = False)
-    advertisements = db.relationship("Advertisement", backref="reason", lazy=True)
+    def __init__(self, category_name):
+        self.category_name = category_name
 
 
-    def __repr__(self):
-        return self.termination_reason
+class CategorySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'category_name')
 
-class ReportReason(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    report_reason = db.Column(db.String(80), unique = True, nullable = False)
-    reports = db.relationship("Report", backref = "reason", lazy = True)
 
-    def __repr__(self):
-        return self.report_reason
+category_schema = CategorySchema()
+categories_schema = CategorySchema(many=True)
+
+
+class ReportReason(enum.Enum):
+    BAD_LANGUAGE = 'Contains offensive language'
+    SCAM = 'Is trying to scam somebody'
+
 
 class Report(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    report_reason = db.Column(db.Integer, db.ForeignKey('report_reason.id') , nullable =  False)
-    advertisement = db.Column(db.Integer, db.ForeignKey('advertisement.id') , nullable =  False)
-    reporter = db.Column(db.Integer, db.ForeignKey('user.id') , nullable =  False)
+    id = db.Column(db.Integer, primary_key=True)
+    report_reason = db.Column(db.Integer, db.Enum(Report_Reason), nullable=False)
+    advertisement = db.Column(db.Integer, db.ForeignKey('advertisement.id'), nullable=False)
+    reporter = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    #TODO: discuss what string should represt report
-    def __repr__(self):
-        return self.report_reason
+
+class EndReason(enum.Enum):
+    SOLD = "Sold"
+    NOT_SOLD = "Not sold"
+
 
 class Advertisement(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(80),nullable = False)
-    description = db.Column(db.String(500),nullable = False)
-    price = db.Column(db.Float (precision='5,2'))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Float(precision='5,2'))
+    is_promoted = db.Column(db.Boolean(), nullable=False)
     photo_path = db.Column(db.String(500))
-    category = db.Column(db.Integer, db.ForeignKey('category.id') , nullable =  False)
-    owner = db.Column(db.Integer, db.ForeignKey('user.id') , nullable =  False)
-    termination_reason = db.Column(db.Integer, db.ForeignKey('termination_reason.id') , nullable =  False)
+    category = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    end_reason = db.Column(db.Integer, db.Enum(End_Reason), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
 
-
-
-    def __repr__(self):
-        return self.title
 
 class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80),nullable = False)
-    surname = db.Column(db.String(80),nullable = False)
-    email = db.Column(db.String(80), unique = True, nullable = False)
-    phone_number = db.Column(db.Integer, unique = True, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+    show_phone = db.Column(db.Boolean(), nullable=False)
     password_hash = db.Column(db.String(128))
-    is_activated = db.Column(db.Boolean(),default = False)
-    is_admin = db.Column(db.Boolean(),default = False)
-    is_moderator = db.Column(db.Boolean(),default = False)
-    to_dlete = db.Column(db.Boolean(),default = False)
-    delete_date= db.Column(db.DateTime())
-    advertisements = db.relationship("Advertisement", backref="owner", lazy=True)
-    messages_send = db.relationship("Message", backref="sender", lazy=True)
-    messages_received = db.relationship("Message", backref="receiver", lazy=True)
+    is_activated = db.Column(db.Boolean(), default=False)
+    is_admin = db.Column(db.Boolean(), default=False)
+    is_moderator = db.Column(db.Boolean(), default=False)
+    delete_date = db.Column(db.DateTime())
 
-    def __repr__(self):
-        return self.name + " " + self.surname
+
+class Favorite(db.Model):
+    ad = db.Column(db.Integer, db.ForeignKey('Advertisement.id'), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    person_a = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    person_b = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 class Message(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    message_text = db.Column(db.String(500),nullable = False)
-    sender = db.Column(db.Integer, db.ForeignKey('user.id') , nullable =  False)
-    receiver = db.Column(db.Integer, db.ForeignKey('user.id') , nullable =  False)
-    advertisement = db.Column(db.Integer, db.ForeignKey('advertisement.id') , nullable =  False)
+    id = db.Column(db.Integer, primary_key=True)
+    message_text = db.Column(db.String(500), nullable=False)
+    message_date = db.Column(db.DateTime, nullable=False)
+    direction = db.Column(db.Boolean(), nullable=False)
+    conversation = db.Column(db.Integer, db.ForeignKey('Conversation.id'), nullable=False)
+
 
 class AppSettings(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    key = db.Column(db.String(80),nullable = False)
-    value = db.Column(db.String(80),nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(80), nullable=False)
+    value = db.Column(db.String(80), nullable=False)
 
 
 @app.route('/')
 def index():
     return 'Wale wiadro'
 
+
+@app.route('/api/category', methods=['POST'])
+def create_category():
+    name = request.json['category_name']
+    new_category = Category(name)
+
+    db.session.add(new_category)
+    db.session.commit()
+
+    return category_schema.jsonify(new_category)
+
+
+@app.route('/api/category', methods=['GET'])
+def get_categories():
+    all_categories = Category.query.all()
+    result = categories_schema.dump(all_categories)
+    return jsonify(result)
+
+
+@app.route('/api/category/<id>', methods=['GET'])
+def get_category(id):
+    category = Category.query.get(id)
+
+    return category_schema.jsonify(category)
+
+
+@app.route('/api/category/<id>', methods=['PUT'])
+def update_category(id):
+    category = Category.query.get(id)
+    category.category_name = request.json['category_name']
+
+    db.session.commit()
+    return category_schema.jsonify(category)
+
+
+@app.route('/api/category/<id>', methods=['DELETE'])
+def delete_category(id):
+    category = Category.query.get(id)
+    db.session.delete(category)
+    db.session.commit()
+    return category_schema.jsonify(category)
+
+
 if __name__ == '__main__':
-    app.run(port=8000,debug=True, host='0.0.0.0')
+    app.run(port=8000, debug=True, host='0.0.0.0')
